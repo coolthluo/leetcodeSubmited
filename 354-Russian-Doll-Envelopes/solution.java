16d64c7c26e55e5de1a87dfcc2ebaad7b07a26f3
Robin Luo <zhengbin.luo@gmail.com>
2016-07-03 00:03:10 -0400

    [354. Russian Doll Envelopes][Compile Error]committed by LeetCode Extension

--- 354-Russian-Doll-Envelopes/solution.java
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -1,19 +1,26 @@
 public class Solution {
     public int maxEnvelopes(int[][] envelopes) {
         int res = 0;
-        if (envelopes.length == 0 || envelopes[0].length == 0) {
-            return res;
+        if (envelopes == null || envelopes.length == 0) {
+            return 0;
+        }
+        if (m == 1 && n == 1) {
+            return 1;
         }
         int m = envelopes.length;
         int n = envelopes[0].length;
         
-        Arrays.sort(envelopes[0]);
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
         
-        for (int i = 0; i < n - 1; i++) {
+        for (int i = 0; i < m - 1; i++) {
             int max = 1;
             int rowValue = envelopes[i][0];
             int colValue = envelopes[i][1];
-            for (int j = i + 1; j < n; j++) {
+            for (int j = i + 1; j < m; j++) {
                 if (rowValue < envelopes[j][0] && colValue < envelopes[j][1]) {
                     max++;
                     res = Math.max(res, max);
