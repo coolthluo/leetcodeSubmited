2eba7d5b3e840f46c9570f366b5616ef7881829d
Robin Luo <zhengbin.luo@gmail.com>
2016-07-07 15:26:36 -0400

    [354. Russian Doll Envelopes][Compile Error]committed by LeetCode Extension

--- 354-Russian-Doll-Envelopes/solution.java
+++ 354-Russian-Doll-Envelopes/solution.java
@@ -1,21 +1,29 @@
 public class Solution {
-    public int maxEnvelopes(int[][] envelopes) {  
-        Arrays.sort(envelopes, new Comparator<int[]>() {  
-            @Override public int compare(int[] e1, int[] e2) {  
-                return e1[0] - e2[0];  
-            }  
-        });  
-        int max = 0;  
-        int[] counts = new int[envelopes.length];  
-        for(int i=0; i<envelopes.length; i++) {  
-            counts[i] = 1;  
-            for(int j=0; j<i; j++) {  
-                if (envelopes[j][0] < envelopes[i][0] && envelopes[j][1] < envelopes[i][1]) {  
-                    counts[i] = Math.max(counts[i], counts[j] + 1);  
-                }  
-            }  
-            max = Math.max(max, counts[i]);  
-        }  
-        return max;  
+    public int maxEnvelopes(int[][] envelopes) {
+        if (envelopes == null || envelopes.length == 0) {
+            return 0;
+        }
+        int res = 1;
+        int m = envelopes.length;
+        int n = envelopes[0].length;
+        
+        
+        Arrays.sort(envelopes, new Comparator<int[]>() {
+            public int compare(int[] a, int[] b) {
+                return Integer.compare(a[0], b[0]);
+            }
+        });
+        
+        for (int i = 0; i < m; i++) {
+            dp[i] = 1;
+            for (int j = 0; j < i; j++) {
+                if (envelopes[i][0] > envelopes[j][0] && envelopes[i][1] < envelopes[j][1]) {
+                    res = Math.max(res, max);
+                    rowValue = envelopes[j][0];
+                    colValue = envelopes[j][1];
+                }
+            }
+        }
+        return res;
     }
 }
