2673b96a95e3df3595500a58c7ac763de44074c7
Robin Luo <zhengbin.luo@gmail.com>
2016-07-10 22:47:48 -0400

    [362. Design Hit Counter][Accepted]committed by LeetCode Extension

--- 362-Design-Hit-Counter/solution.java
+++ 362-Design-Hit-Counter/solution.java
@@ -1,24 +1,20 @@
 public class HitCounter {
-    Deque<Integer> queue;
+
     /** Initialize your data structure here. */
     public HitCounter() {
-        queue = new LinkedList<>();
         
     }
     
     /** Record a hit.
         @param timestamp - The current timestamp (in seconds granularity). */
     public void hit(int timestamp) {
-        queue.offer(timestamp);
+        
     }
     
     /** Return the number of hits in the past 5 minutes.
         @param timestamp - The current timestamp (in seconds granularity). */
     public int getHits(int timestamp) {
-        while (!queue.isEmpty() && timestamp - queue.peekFirst() >= 300) {
-            queue.poll();
-        }
-        return queue.size();
+        
     }
 }
 
