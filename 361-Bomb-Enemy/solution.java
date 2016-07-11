fcce61beac8a587b15e16e5de2822ae74935fa9e
Robin Luo <zhengbin.luo@gmail.com>
2016-07-11 16:40:49 -0400

    [361. Bomb Enemy][Accepted]committed by LeetCode Extension

--- 361-Bomb-Enemy/solution.java
+++ 361-Bomb-Enemy/solution.java
@@ -10,10 +10,25 @@ public class Solution {
         int[] colCache = new int[col];
         for (int i = 0; i < row; i++) {
             for (int j = 0; j < col; j++) {
+                // if (j == 0 || grid[i][j - 1] == 'W') {
+                //     rowCache = 0;//每次在边界或者上一个位置是'W'时，之前的一行的杀敌数就不能再累积了
+                //     for (int k = j; k < col && grid[i][k] != 'W'; k++) {
+                //         rowCache += grid[i][k] == 'E' ? 1 : 0;
+                //     }
+                // }
+                // if (i == 0 || grid[i - 1][j] == 'W') {
+                //     colCache[j] = 0;
+                //     for (int k = i; k < row && grid[k][j] != 'W'; k++) {
+                //         colCache[j] += grid[k][j] == 'E' ? 1 : 0;
+                //     }
+                // }
+                // if (grid[i][j] == '0') {
+                //     res = Math.max(res, rowCache + colCache[j]);
+                // }
                 if (j == 0 || grid[i][j - 1] == 'W') {
                     rowCache = 0;
                     for (int k = j; k < col && grid[i][k] != 'W'; k++) {
-                        rowCache += grid[i][k] == 'E' ? 1 : 0;
+                        rowCache +=  grid[i][k] == 'E' ? 1 : 0;
                     }
                 }
                 if (i == 0 || grid[i - 1][j] == 'W') {
